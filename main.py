406a080b31da0c701bc9b6653e94040518df6444
zong.xiangrui <zxr@tju.edu.cn>
2022-02-26 20:56:45 +0800

    增加包需求
    完善requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 import pandas as pd
-
-
-if __name__ == '__main__':
-    cons_info = pd.read_csv("./data/训练组_比特币挖矿_档案明细（20211220）.csv", index_col=0, quotechar='"', encoding="gbk")
-    elec_day = pd.read_csv("./data/训练组_比特币挖矿_日用电明细（20211217）.csv")
-    elec_month = pd.read_csv("./data/训练组_比特币挖矿_月用电明细（20211217）.csv")
-    target = pd.read_csv("./data/训练组_比特币挖矿_疑似用户明细（20211217）.csv ", index_col="id")
-    print(target.head())
+import time
+from datetime import datetime
+from tqdm import tqdm
+from time import strftime, localtime
+from scipy import stats
+from scipy.spatial import distance
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from lightgbm import LGBMClassifier
+from joblib import dump, load
+from tensorflow.keras.backend import epsilon
+from itertools import combinations
+from imblearn.over_sampling import SMOTE, ADASYN
 
 
